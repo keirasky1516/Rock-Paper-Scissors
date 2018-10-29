3a41dd39d482af2d7cecf80f7c849792850664e1
Keira <keira.plybon.2021@student.lhschools.org>
2018-10-29 08:13:57 -0400

    Start of project

---
+++ src/RockPaperScissors.java
@@ -0,0 +1,67 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissors {
+    public static void main(String[] args){
+        int input;
+        Scanner keyboard = new Scanner(System.in);
+        Random rnd = new Random();
+        System.out.println("Welcome to Rock Paper Scissors! Would you like to play?");
+        keyboard.nextLine();
+        System.out.println("Select 1, 2, 3, for Rock, Paper, Scissors");
+        keyboard.nextInt();
+
+        
+
+
+
+
+
+    }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        
